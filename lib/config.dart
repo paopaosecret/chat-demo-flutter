ae9243f2dbe16a4cfe1961d658afd11645b65455
xanderzhao <xanderzhao@tencent.com>
2024-02-22 15:55:19 +0800

    【ChatDemo】跑通 Android Demo

--- lib/config.dart
+++ lib/config.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: constant_identifier_names
 
+import 'package:tencent_cloud_chat_demo/generate_test_user_sig.dart';
+
 class IMConfig {
   /// Please specify the following fields before using this sample app
-  static String sdkappid = "";
-  static String userid = "";
-  static String usersig = "";
+  static String sdkappid = GenerateTestUserSig.sdkAppId.toString();
+  static String userid = "123666";
+  static String usersig = GenerateTestUserSig.genTestSig("123666");
   static const appVersion = "2.0.0";
 }
